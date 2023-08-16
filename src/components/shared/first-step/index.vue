30d9b40d68322ec99304a4c042bb4fdabdbcb8db
Elisaveta <chemical.orange.web@gmail.com>
2023-08-16 16:03:08 +0300

    Initial commit

--- src/components/shared/first-step/index.vue
+++ src/components/shared/first-step/index.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import IconTriangle from "@/components/icons/IconTriangle.vue";
 import ButtonEl from "@/components/ui/button/index.vue";
 import {useGameStore} from "@/stores/game";
--- src/components/shared/second-step/index.vue
+++ src/components/shared/second-step/index.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import ButtonEl from "@/components/ui/button/index.vue"
 import { onMounted } from "vue"
 import { useGameStore } from "@/stores/game"
