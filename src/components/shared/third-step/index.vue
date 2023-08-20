75169e0e13a542d29c0284c01e91ef50f4063030
Elisaveta <chemical.orange.web@gmail.com>
2023-08-20 16:36:28 +0600

    --feature (win animation)

--- src/components/shared/third-step/index.vue
+++ src/components/shared/third-step/index.vue
@@ -8,12 +8,16 @@ const store = useGameStore()
     <div :class="$style.row">
       <div :class="$style.column">
         <h3>You picked</h3>
-        <ButtonEl :variant="store.activeTool" />
+        <ButtonEl :variant="store.activeTool" :class="{ [$style.win]: store.win }" />
       </div>
       <div :class="$style.column">
         <h3>The house picked</h3>
         <div v-if="!store.houseActiveTool" :class="$style.house"></div>
-        <ButtonEl v-if="store.houseActiveTool" :variant="store.houseActiveTool" />
+        <ButtonEl
+          v-if="store.houseActiveTool"
+          :variant="store.houseActiveTool"
+          :class="{ [$style.win]: !store.win }"
+        />
       </div>
     </div>
     <div :class="$style.again">
@@ -31,6 +35,26 @@ const store = useGameStore()
 @media screen and (max-width: 600px) {
   .row {
     gap: 40px;
+  }
+}
+
+.win {
+  animation: win-animation linear 0.3s forwards;
+}
+
+@keyframes win-animation {
+  0% {
+    box-shadow:
+      1px 5px 0 0 rgba(255, 255, 255, 0.08),
+      1px 5px 0 0 rgba(255, 255, 255, 0.04),
+      1px 5px 0 0 rgba(255, 255, 255, 0.02);
+  }
+
+  100% {
+    box-shadow:
+      1px 5px 0 30px rgba(255, 255, 255, 0.08),
+      1px 5px 0 60px rgba(255, 255, 255, 0.04),
+      1px 5px 0 90px rgba(255, 255, 255, 0.02);
   }
 }
 
